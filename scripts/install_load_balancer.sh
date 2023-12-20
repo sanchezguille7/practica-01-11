8837b1d1dff61b57742bbb8275714b39d7d30372
Ubuntu <ubuntu@ip-172-31-93-10.ec2.internal>
2023-12-20 12:31:18 +0000

    load balancer

--- scripts/install_load_balancer.sh
+++ scripts/install_load_balancer.sh
@@ -2,16 +2,18 @@
 
 set -ex
 
+source .env
+
 apt update
 
 apt upgrade -y
 
 apt install apache2 -y
 
-cp ../load-balancer.conf /etc/apache2/sites-available
+cp /home/ubuntu/practica-01-11/conf/load-balancer.conf /etc/apache2/sites-available
 
-sed -i "s/$IP_HTTP_SERVER1/" /etc/apache/sites-available/load-balancer.conf
-sed -i "s/$IP_HTTP_SERVER2/" /etc/apache/sites-available/load-balancer.conf
+sed -i "s/IP_HTTP_SERVER1/$IP_HTTP_SERVER1/" /etc/apache2/sites-available/load-balancer.conf
+sed -i "s/IP_HTTP_SERVER2/$IP_HTTP_SERVER2"/ /etc/apache2/sites-available/load-balancer.conf
 
 a2enmod proxy
 
